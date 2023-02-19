de371a216cf7b399e7432811d9baab9f2c2b399c
fivan14 <ivucetic7792@gmail.com>
2023-02-19 22:55:53 +0100

    added the for loop in function

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const mainDiv = document.querySelector('#main-div')
+const form = document.querySelector('#form')
+const input = document.querySelector('#input-value')
 
-const gridNum = Number.parseInt(prompt(),10)
+//const gridNum = Number.parseInt(,10)
 
-let divHeight = 460 / gridNum
-console.log(divHeight)
-
-for (let i = 0; i < gridNum; i++) {
-    for (let j = 0; j < gridNum; j++) {
-        mainDiv.insertAdjacentHTML('beforeend', `<div id='grid' style='background-color: white; height: ${divHeight - 2}px; width: ${divHeight - 2}px; border: 1px solid black;margin:0px; padding: 0px;'></div>`)
-    } 
-}
 
 console.log(document.querySelectorAll('div').length)
+
+function addDivs(gridSize) {
+    mainDiv.innerHTML = ''
+    let divHeight = 300 / gridSize
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            mainDiv.insertAdjacentHTML('beforeend', `<div id='grid' style='background-color: white; height: ${divHeight - 2}px; width: ${divHeight - 2}px; border: 1px solid black;margin:0px; padding: 0px;'></div>`)
+        }
+    }
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    
+
+    addDivs(input.value)
+})
