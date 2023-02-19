51ab5e6999ad09d7f204f5ee9b44fd8f9f9d3a91
fivan14 <ivucetic7792@gmail.com>
2023-02-20 00:43:41 +0100

    Create events and logic & changing backgroundColor

--- script.js
+++ script.js
@@ -7,19 +7,63 @@ const input = document.querySelector('#input-value')
 
 console.log(document.querySelectorAll('div').length)
 
+//function that add's divs
+
 function addDivs(gridSize) {
     mainDiv.innerHTML = ''
-    let divHeight = 300 / gridSize
+    if (gridSize > 64) {
+        return alert('Hey, max grid size is 64x64!')
+    }
+    let divHeight = 301 / gridSize
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
-            mainDiv.insertAdjacentHTML('beforeend', `<div id='grid' style='background-color: white; height: ${divHeight - 2}px; width: ${divHeight - 2}px; border: 1px solid black;margin:0px; padding: 0px;'></div>`)
+            mainDiv.insertAdjacentHTML('beforeend', `<div class='grid' style='background-color: white; height: ${divHeight - 2}px; width: ${divHeight - 2}px; border: 1px solid black;'></div>`)
         }
     }
 }
+// function that changes background color on hover
+
+function onHover(target) {
+    if (target.classList.contains('grid')) {
+        console.log('Hooray')
+        console.log(target)
+        target.style.backgroundColor = 'gray'
+    }
+}
+
+function onLeave(target) {
+    console.log('mouse left')
+    if (target.classList.contains('grid')) {
+        console.log('Hooray')
+        console.log(target)
+        setTimeout(() => {
+            target.style.backgroundColor='white'
+        }, 1000)
+        
+    }
+}
+
+// add event listener for grid layout
 
 form.addEventListener('submit', event => {
     event.preventDefault()
-    
-
     addDivs(input.value)
+})
+
+
+//console.log(Number.parseInt(mainDiv.style.width, 10))
+
+// event listener for change color on mouseover
+
+mainDiv.addEventListener('mouseover', event => {
+    let element = event.target
+    console.log(element.classList.contains('grid'))
+    onHover(element)
+
+})
+//back to original color on mouse out
+
+mainDiv.addEventListener('mouseout', event => {
+    let element = event.target
+    onLeave(element)
 })
